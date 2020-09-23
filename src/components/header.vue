750856f0e51ce6b6a6323f84af72c6d838b87b08
Arsalan <shiro1702@gmail.com>
2020-09-23 20:52:26 +0700

    Анимация заголовка в шапке

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,20 @@
 <template>
-    <header class="header a-container">
+    <header 
+        class="header a-container"
+        :class="'_' + pageName" 
+    >
         <router-link 
             :to="{name: 'main'}" 
-            class="header__link" 
-            :class="pageName" 
+            class="header__link-main" 
+            :class="'_' + pageName" 
             key="contacts"
         >
+            <svg 
+                v-if="pageName != 'main'"
+                class="svg-icon"
+            >
+                <use xlink:href="#left"></use>
+            </svg>
             Контакты
         </router-link>
     </header>
@@ -24,5 +33,28 @@ export default {
 <style lang="sass">
 .header
     display: flex
-
+    // transition: all .3s
+    // text-align: center
+    &::before
+        content: ''
+        width: 100%
+        transition: all .3s
+    &::after
+        content: ''
+        width: 100%
+        transition: all .3s
+    &__link-main
+        font-size: 16px
+        transition: all .3s
+        display: flex
+        align-items: center
+        // display: inline-block
+        &._main
+            font-size: 32px
+    // &._main
+        // margin: 0 auto
+        // margin-left: auto
+    &:not(._main)
+        &::before
+            width: 0px
 </style>
