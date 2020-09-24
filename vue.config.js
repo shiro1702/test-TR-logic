4784edce02b32e15937d333e5d83485c23f87bd6
Arsalan <shiro1702@gmail.com>
2020-09-24 22:04:44 +0700

    Публикация в github-pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/test-TR-logic/'
+    : '/',
   css: {
     loaderOptions: {
       // передача настроек в sass-loader
